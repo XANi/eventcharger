d1b078a96bfeff16145aa8a0d163f086a51a06e9
Mariusz Gronczewski <xani666@gmail.com>
2018-09-19 01:18:39 +0200

    decode json, handle json errors

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [org.clojure/clojure "1.8.0"]
                  [clj-http "2.0.0"]
                  [io.forward/yaml "1.0.9"]
-                 [clojurewerkz/machine_head "1.0.0"]]
+                 [clojurewerkz/machine_head "1.0.0"]
+                 [cheshire "5.8.0"]
+                 ]
   :main ^:skip-aot eventcharger.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/eventcharger/core.clj
+++ src/eventcharger/core.clj
@@ -1,7 +1,10 @@
 (ns eventcharger.core
   (:use [clojure.pprint])
   (:require [yaml.core :as yaml]
-            [clojurewerkz.machine-head.client :as mqtt]))
+            [clojurewerkz.machine-head.client :as mqtt]
+            [cheshire.core :as json]
+            )
+  (:import (com.fasterxml.jackson.core JsonParseException)))
 
 
 
@@ -12,6 +15,40 @@
 
 ;(require '[clojurewerkz.machine-head.client :as mqtt])
 
+;(defn event-handler
+(defn event-handler [^String topic _ ^bytes payload]
+  (print (str topic) ": ")
+  (let [payload-str (apply str (map char payload))]
+    (pprint (json/parse-string payload-str))
+
+    )
+  )
+(defn handle-json-err [e json]
+  (println "error parsing JSON string ["  (String. json  "UTF-8")"]): ")
+  (println (.getMessage e))
+  ;(println (type e))
+  ;(pprint e)
+  )
+(defn handle-generic-err [e]
+  (println (.getMessage e))
+  (println (type e))
+  ;(pprint e)
+  )
+
+
+
+(defn event-handler-ex [^String topic _ ^bytes payload]
+  (print (str topic) ": ")
+  (try
+    (let [payload-str (String. payload "UTF-8")]
+       (pprint (json/parse-string payload-str)))
+    (catch JsonParseException e (handle-json-err e payload))
+    (catch Throwable e  (handle-generic-err e))
+    )
+      ;(finally (println "bad json")) ;  this is ran regardless of try
+  )
+
+
 
 
 (def conn (mqtt/connect (:address (:default (:bus config)))
@@ -20,9 +57,13 @@
                           :password (:pass (:default (:bus config)))}}))
 
 
-(mqtt/subscribe conn {"metrics/+" 0}
-                (fn [^String topic _ ^bytes payload]
-                  (print (str topic ": ")
-                         (println (apply str ((map char payload)))))))
+(mqtt/subscribe conn {"metrics/#" 0} event-handler-ex)
+;(mqtt/subscribe conn {"events/#" 0 "metrics/#" 0} event-handler-ex)
+                ;(fn [^String topic _ ^bytes payload]
+                ;  (print (str topic) ": ")
+                ;   (pprint (json/parse-string (apply str (map char payload))))
+                ;    ))
 
-(mqtt/publish conn "discovery" (str (ns-name *ns*)))
+;                  (print (str topic) ": ")
+;                         (println (apply str (map char payload)))))
+;(mqtt/publish conn "discovery" (str (ns-name *ns*)))
